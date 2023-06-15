d518db2f16c97f33d15c3b5efa6f4a862889549f
Peter Tamvakis <petrostam77@gmail.com>
2023-06-15 10:45:08 -0500

    Commit 2.0

--- test_code.py
+++ test_code.py
@@ -1,5 +1,5 @@
 def greetings(name):
-    print(f'Hello {name}! How are you today?')
+    print(f'Hello {name}! How are you today? Hope you are fine!')
 
 if __name__ == '__main__':
     greetings('Peter')
